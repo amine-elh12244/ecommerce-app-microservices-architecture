9d2371304b54b8bca700f040c269c6537387589e
amine-elh12244 <amineelhmid01@gmail.com>
2024-10-09 10:10:13 +0100

    Commit Application

---
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,35 @@
+package org.sid.inventoryservice;
+
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+		return args -> {
+			restConfiguration.exposeIdsFor(Product.class);
+			Stream.of("Ordinateur","Imprimante","Smartphone").forEach(p->{
+				productRepository.save(Product.builder()
+						.name(p)
+						.price(1000+Math.random()*9000)
+						.quantity(3+(int)(Math.random()*100))
+						.build());
+			});
+
+		};
+	}
+
+}
